a79cbc249ce50ab8d67935083c0c5204487f5bd4
Leandro-3016 <melodias309@gmail.com>
2026-05-21 16:40:43 -0300

    Add files via upload

---
+++ Aula 2.py
@@ -0,0 +1,17 @@
+#nome = input("Digite seu nome")
+#idade = input("Digite sua idade")
+#print (f'{nome} tem {idade} anos')
+
+lista = [20,1,2,3,4,5,7,8,10,15,13,12,14,11]
+
+lista.insert(0,0)
+
+lista.remove(4)
+
+lista.sort()
+
+lista.pop()
+
+print(lista)
+
+
